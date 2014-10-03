711493112bbcd8860e4ea87f9c94dd70ec2cffa2
Jesper Eskilson <jesper.eskilson@iar.com>
2014-10-03 15:19:59 +0200

    Work in progress

--- src/jvm.erl
+++ src/jvm.erl
@@ -13,17 +13,16 @@ jvm(InitClass, ClassPath) ->
 
 %% Load class and execute initialization method
 initialize_class(JVM, Class) ->
-    ClassInitMethod = classfile:lookup_method("<clinit>", "()V", Class#class.classfile),
+    CF = Class#class.classfile,
+    ClassInitMethod = classfile:lookup_method("<clinit>", "()V", CF),
     InitThread = #javathread{current_method = ClassInitMethod, 
 			     frames = [#frame{}]},
-    execute(JVM, Class, InitThread).
-
-execute(_JVM, Class, JavaThread) ->
-    Code = classfile:get_method_code(JavaThread#javathread.current_method, Class#class.classfile),
-    ?debugFmt("~nExecuting: ~w~n", [Code]),
+    Code = classfile:get_method_code(ClassInitMethod, CF),
+    ?debugFmt("~nExecuting: ~w~n", [Code]).
     
 %%%
 %%% Unit tests.
+
 jvm_test() ->
     jvm("Test", ["../priv"]).
 
